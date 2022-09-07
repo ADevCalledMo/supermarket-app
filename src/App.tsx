a8aa7d983729f2d2631bd1447013109afb0507b4
ADevCalledMo <mozeswalker@outlook.com>
2022-09-07 16:33:07 +0100

    altered ts config, now no linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
 import React, { useState } from "react";
 import { IntlProvider, FormattedMessage } from "react-intl";
-import messages from "./data/messages";
+
 import "./styles/index.scss";
 
 function App() {
-  const [locale, setLocale] = useState("English");
-
-  const handleLocaleChange = (event: any) => {
-    setLocale(event.target.value);
-  };
-
-
-
-  // const result = messages[locale as keyof typeof messages];
-  // console.log(result, "<--- result");
-
-
-
-  return (
-    <div className="App">
-      <select onChange={handleLocaleChange} defaultValue={locale}>
-        {["English", "日本語"].map((lang) => (
-          <option key={lang}>{lang}</option>
-        ))}
-      </select>
-
-      <IntlProvider locale={locale} messages={messages[locale]}>
-        <FormattedMessage
-          id="Home"
-          defaultMessage="Home"
-          values={{ locale }}
-        ></FormattedMessage>
-      </IntlProvider>
-    </div>
-  );
+  return <>Hello</>;
 }
 
 export default App;
---
+++ src/translate.ts
@@ -0,0 +1,53 @@
+import { createIntl, createIntlCache } from "react-intl";
+
+// English and Japanese messages translated
+export const messages = {
+  English: {
+    Hello: "Hello",
+    Home: "Home",
+    About: "About",
+  },
+  Japanese: {
+    Hello: "こんにちは",
+    Home: "ホーム",
+    About: "アバウト",
+  },
+};
+
+// Prevents Memory Leak
+const cache = createIntlCache();
+
+// Allows Formatting of things outside of the Provider...
+// has cache to prevent memory leak
+// makes default language English
+let int = createIntl(
+  {
+    locale: "English",
+    messages: messages["English"],
+  },
+  cache
+);
+
+// Another createIntl function to change the language..
+// can be used outside of file
+// changes int on line 24 to whatever new langauge is...
+export function changeLanguage(lang: string) {
+  const newIntl = createIntl(
+    {
+      locale: lang,
+      messages: messages[lang],
+    },
+    cache
+  );
+  int = newIntl;
+}
+
+// Translates whatever the Intl is
+// takes in id (which is a string)
+// takes in values (which is an object)
+// returns a formatted message based on what was passed in
+const translate = (id: string, values?: {}) => {
+  return int.formatMessage({ id }, values);
+};
+
+export default translate;
