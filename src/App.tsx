85a9ae34dfc2e740e941a545200387f43337b220
ADevCalledMo <mozeswalker@outlook.com>
2022-09-13 22:23:48 +0100

    Started first apple Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IntlProvider } from "react-intl";
 import { messages } from "./translate";
+import { products } from "./data/products";
 import Navbar from "./components/Navbar";
 import "./styles/index.scss";
 
@@ -10,6 +11,21 @@ function App() {
   return (
     <IntlProvider locale={lang} messages={messages[lang]}>
       <Navbar language={lang} handleLangChange={setLang} />
+      <div className="product">
+        <div className="products__text">
+          <img src={products.img} alt="Apple" />
+          <h2>{products.name}</h2>
+          <p>{products.description}</p>
+        </div>
+        <div className="products__buttons__quantity">
+          <button>+</button>
+          <span>0</span>
+          <button>-</button>
+        </div>
+        <div className="products__addToBasket">
+          <button>Add to Basket</button>
+        </div>
+      </div>
     </IntlProvider>
   );
 }
---
+++ src/data/products.ts
@@ -0,0 +1,5 @@
+export const products = {
+    name: "Apple", 
+    description: "Golden Delicious",
+    img: "https://cdn.iconscout.com/icon/free/png-256/red-apple-fruit-emoj-symbol-food-30677.png"
+  };
