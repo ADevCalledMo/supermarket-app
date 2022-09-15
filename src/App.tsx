32575988d49d90a578da0d46ff74103eda03b068
ADevCalledMo <mozeswalker@outlook.com>
2022-09-15 21:04:38 +0100

    added translate to add basket and increment/decement to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ import "./styles/index.scss";
 
 function App() {
   const [lang, setLang] = useState("English");
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
 
   return (
     <IntlProvider locale={lang} messages={messages[lang]}>
@@ -21,12 +30,16 @@ function App() {
             <p>{translate("AppleDesc")}</p>
           </div>
           <div className="products__buttons__quantity">
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
+            <button onClick={handleIncrement} disabled={count === 9}>
+              +
+            </button>
+            <span>{count}</span>
+            <button onClick={handleDecrement} disabled={count === 0}>
+              -
+            </button>
           </div>
           <div className="products__addToBasket">
-            <button>Add to Basket</button>
+            <button>{translate("AddToBasket")}</button>
           </div>
         </div>
       </div>
