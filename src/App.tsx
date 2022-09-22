e94d47e205792e46b9c765eabc0ff4e7182931bf
ADevCalledMo <mozeswalker@outlook.com>
2022-09-22 17:34:11 +0100

    added another product + button styles

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,48 @@ function App() {
             <p>{translate("AppleDesc")}</p>
           </div>
           <div className="products__buttons__quantity">
-            <button onClick={handleIncrement} disabled={count === 9}>
+            <button
+              className="button__quantity"
+              onClick={handleIncrement}
+              disabled={count === 9}
+            >
               +
             </button>
             <span>{count}</span>
-            <button onClick={handleDecrement} disabled={count === 0}>
+            <button
+              className="button__quantity"
+              onClick={handleDecrement}
+              disabled={count === 0}
+            >
+              -
+            </button>
+          </div>
+          <div className="products__addToBasket">
+            <button>{translate("AddToBasket")}</button>
+          </div>
+        </div>
+        <div className="product">
+          <div className="products__text">
+            <div className="product__image">
+              <img src={productImages.AppleImage} alt="Apple" />
+            </div>
+            <h2>{translate("Apple")}</h2>
+            <p>{translate("AppleDesc")}</p>
+          </div>
+          <div className="products__buttons__quantity">
+            <button
+              className="button__quantity"
+              onClick={handleIncrement}
+              disabled={count === 9}
+            >
+              +
+            </button>
+            <span>{count}</span>
+            <button
+              className="button__quantity"
+              onClick={handleDecrement}
+              disabled={count === 0}
+            >
               -
             </button>
           </div>
